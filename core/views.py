92c005fcbe8dfdc656b6677eae5deb56ec95fd3e
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-02-22 15:33:29 -0300

    feat: Add event list page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from core.models import Evento
 
 # Create your views here.
+
+def lista_eventos(request):
+    #usuario = request.user
+    #eventos = Evento.objects.filter(usuario=usuario)
+    eventos = Evento.objects.all()
+    dados = {'eventos': eventos}
+    print(dados)
+    return render(request, 'agenda.html', dados)
