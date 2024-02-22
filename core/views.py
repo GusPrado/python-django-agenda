cf9c879bfdd439d10d32b156680d5b0b50d79645
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-02-22 17:01:48 -0300

    feat:  Add auth methods & login screen

--- core/views.py
+++ core/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
-from django.template.loader import render_to_string
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from core.models import Evento
 
 # Create your views here.
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def login_submit(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuário e/ou senha inválidos')
+
+    return redirect('/')
+    
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    #usuario = request.user
-    #eventos = Evento.objects.filter(usuario=usuario)
-    eventos = Evento.objects.all()
+    usuario = request.user
+    eventos = Evento.objects.filter(usuario=usuario)
+    # eventos = Evento.objects.all()
     dados = {'eventos': eventos}
     print(dados)
     return render(request, 'agenda.html', dados)
